a862c16f0663c6461afccb84147d7f9652a83ec1
Rodrigo Assirati Dias <rodrigodias@gmail.com>
2025-08-24 22:09:20 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 input.onPinPressed(TouchPin.P0, function () {
-    datalogger.log(datalogger.createCV("Jogar bola ou esportes", 0))
+    datalogger.log(datalogger.createCV("Jogar bola ou esportes", 1))
 })
 input.onPinPressed(TouchPin.P2, function () {
-    datalogger.log(datalogger.createCV("Brincar de outra coisa", 0))
+    datalogger.log(datalogger.createCV("Brincar de outra coisa", 1))
 })
 input.onPinPressed(TouchPin.P1, function () {
-    datalogger.log(datalogger.createCV("Conversar com os amigos", 0))
+    datalogger.log(datalogger.createCV("Conversar com os amigos", 1))
 })
 datalogger.setColumnTitles(
 "Jogar bola ou esportes",
